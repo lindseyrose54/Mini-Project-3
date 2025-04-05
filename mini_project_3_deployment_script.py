0ea18d070e60f5dd16595a2e5410c1a515bae3fb
lindseyrose54 <141650585+lindseyrose54@users.noreply.github.com>
2025-04-04 22:02:40 -0700

    Create mini_project_3_deployment_script.py

---
+++ mini_project_3_deployment_script.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import joblib
+
+model = joblib.load('/lstm_model.pkl')
+stacking_model = joblib.load('/mini_project.pkl')
+
+st.title('Hotel Cancelation rates based off economic triggers')
+st.write('Predictions utilizing stacking ensemble and LSTM')
+
+Days_til_booking = st.number_input('Enter number of days between booking and check out')
+Month_of_arrival = st.number_input('Enter the month of arrival (0-12)')
+GDP = st.number_input('Enter GDP')
+Intrest_rate = st.number_input('Enter Interest Rate')
+Inflation_chg = st.number_input('Enter Inflation Change')
+Inflation = st.number_input('Enter Inflation')
+CPI_avg = st.number_input('Enter CPI Average')
+CPI_hotels = st.number_input('Enter CPI Hotels')
+Fuel_prc = st.number_input('Enter average Fuel Price')
+Unemployment_rate = st.number_input('Enter Unemployment Rate')
+
+if st.button('Predict Cancelation Rate'):
+  features = [[Days_til_booking, Month_of_arrival, GDP, Intrest_rate, Inflation_chg, Inflation, CPI_avg, CPI_hotels, Fuel_prc, Unemployment_rate]]
+  
+  prediction_stack = stacking_model.predict([features])[0]
+  prediction_LSTM = model.predict([features])[0]
+
+  st.write(f'Stacking Ensemble Prediction: ${prediction_stack}')
+  st.write(f'LSTM Prediction: ${prediction_LSTM}')
