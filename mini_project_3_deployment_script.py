f1eb2e537e44c87ebac6dde213f54a4e7f048ca9
Lindsey Rogers <141650585+lindseyrose54@users.noreply.github.com>
2025-04-19 07:58:50 -0700

    Update mini_project_3_deployment_script.py

--- mini_project_3_deployment_script.py
+++ mini_project_3_deployment_script.py
@@ -33,7 +33,7 @@ if st.button('Predict Cancelation Rate'):
     prediction_LSTM = model.predict(features)[0][0]
 
     # Display prediction
-   if prediction_LSTM > 0.5:
+if prediction_LSTM > 0.5:
     st.markdown(f"<span style='color:red'>Prediction: Guest will likely cancel (Score: {prediction_LSTM:.2f})</span>", unsafe_allow_html=True)
 else:
     st.markdown(f"<span style='color:green'>Prediction: Guest will likely not cancel (Score: {prediction_LSTM:.2f})</span>", unsafe_allow_html=True)
